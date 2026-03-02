e8538cdcb71d29f766688c5c46922143a31760c6
Evelyn Villarreal <up230772@alumnos.upa.edu.mx>
2026-03-01 21:24:03 -0600

    Tarea 5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,52 @@
 module Helper exposing (..)
+
+
+type Grade
+    = Approved
+    | Failed
+    | Pending
+
+
+type AirplaneStatus
+    = Cancelled
+    | Delayed
+    | OnTime
+    | Boarding
+
+
+categoricalGrade : List Float -> List Grade
+categoricalGrade grades =
+    List.map gradeCategory grades
+
+
+gradeCategory : Float -> Grade
+gradeCategory g =
+    if g >= 7 then
+        Approved
+
+    else if g >= 0 then
+        Failed
+
+    else
+        Pending
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
